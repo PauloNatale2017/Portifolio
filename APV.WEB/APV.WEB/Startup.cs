6e6d82380d92d65f0b252849828fe3b9fc313b60
GRUPOLINCES\paulo.natale <paulo.natale@dekra.com>
2019-05-28 16:53:32 -0300

    firts

---
+++ APV.WEB/APV.WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(APV.WEB.Startup))]
+namespace APV.WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
